a907fab2efaca0080d074750837d0fad7b275da4
Juliana Connors <juliana.g.connors@gmail.com>
2020-07-21 16:08:51 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,25 @@
 # code here!
+class School
+  attr_accessor :roster
+  attr_reader :name
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  def add_student(student_name, student_grade)
+    roster[student_grade] ||= []
+    roster[student_grade] << student_name
+  end
+  
+ def grade(grade_num)
+  return roster[grade_num]
+ end
+  
+  def sort
+    roster.collect do |grade, student_array|
+      roster[grade] = student_array.sort
+        return roster
+      end
+    end
+  end
