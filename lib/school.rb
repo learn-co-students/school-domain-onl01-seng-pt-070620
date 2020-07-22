4aaffa4ea2cce99df4f9561d55b4e55b5834b5bc
Jacob Viti <thevitij64@gmail.com>
2020-07-22 01:45:37 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
 # code here!
+class School
+  attr_accessor :name, :roster
+
+  def initialize(school)
+    @school = school
+    @roster = {}
+  end
+
+  def add_student(student, grade)
+    if !roster.include?(grade)
+    roster[grade] = []
+  end
+    roster[grade] << student
+  end
+
+  def grade(grade)
+    return roster[grade]
+  end
+
+  def sort
+    sortedHash = {}
+    roster.each do |grade, student|
+      sortedHash[grade] = student.sort
+    end
+    return sortedHash
+  end
+
+end
