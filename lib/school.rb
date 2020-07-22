881e496fcd43824c95ab1ba3df56d584dcc8f77f
Nasrin Rahimi <77.nasrin@gmail.com>
2020-07-22 22:26:28 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,34 @@
-# code here!
+require 'pry'
+class School
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  def roster
+    @roster
+  end
+  
+  def add_student(name,grade)
+    if !(@roster.key?(grade))
+      @roster[grade]  = []
+    end
+    @roster[grade] << name
+  end
+  
+  def grade(grade)
+    @roster[grade]
+  end
+  
+  def sort
+    sorted_roster = {}
+    @roster.each do |grade,students|
+      sorted_roster[grade] = students.sort
+    end
+    sorted_roster
+  end
+  
+end
+
+school = School.new("Bayside High School")
