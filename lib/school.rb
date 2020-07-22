8b97b028c0ef78f9fcd2641d1c51563bf7000e88
Andy Adams <andrewdcadams@gmail.com>
2020-07-22 16:10:53 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,40 @@
 # code here!
+
+class School
+  
+  attr_accessor :roster
+  attr_reader :name
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  
+  def add_student (name, grade)
+    if @roster.include?(grade) == false
+      @roster[grade] = []
+    end
+    @roster[grade] << name
+  end
+  
+  def roster
+    @roster
+  end
+  
+  def grade(grade)
+    @roster[grade]
+  end
+  
+  def sort 
+    @roster.collect do |key, value|
+      puts "key is #{key}"
+      puts "before #{value} "
+      
+      value = value.sort
+      
+      puts "after  #{value} "
+    end
+  @roster
+  end
+end #ends class
