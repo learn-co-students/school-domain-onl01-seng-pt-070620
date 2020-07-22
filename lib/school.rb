20bd958501078f069c3e52fd6831e3767eefe844
Katie Le <lethikatie@gmail.com>
2020-07-22 02:08:32 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,29 +1,28 @@
-class School
+class School 
   def initialize(name)
-   @name = name 
-   @roster = {}
- end 
- 
-  def roster
-    @roster
- end 
- 
-  def add_student(name, grade)
-    if @roster[grade] != nil
-      @roster[grade] << name
-    else
-      @roster[grade] = [name]
-    end
-  end
- 
-  def grade(grade) 
-    @roster[grade]
-  end 
-  
-  def sort
-    @roster.each do |key, value|
-      value.sort!
-    end
-  end
+    @name = name 
+    @roster = {}
 end 
 
+  def roster
+    @roster
+  end 
+  
+  def add_student(name, grade)
+    if self.roster[grade]
+      self.roster[grade] << name
+    else 
+      self.roster[grade] = [name]
+    end 
+  end 
+  
+  def grade(grade)
+    roster[grade]
+end 
+
+def sort 
+  self.roster.each do |name, grade|
+    grade.sort!
+   end 
+  end 
+end 
