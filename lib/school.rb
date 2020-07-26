3b9aaf00751bac309b8d705738bd1a5191e1c47a
Billy Gunn, Jr <bgunnjr@gmail.com>
2020-07-26 05:37:02 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,18 @@
-# code here!
+class School
+  attr_accessor :grade, :roster
+  attr_reader :name
+  
+  def initialize(name)
+    @name = name
+    @roster = roster
+  end
+  
+  def roster
+    @roster
+  end
+  
+  def add_student
+    @roster "#{name", #{grade}
+  end
+end
+
