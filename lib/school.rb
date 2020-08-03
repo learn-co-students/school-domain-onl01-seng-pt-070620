2da8e565766b8af0304a67ed4983fb840427ca3d
Fatou Diagne <f.m.diagne1@gmail.com>
2020-08-03 18:51:10 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,20 @@
-# code here!
+class School
+  attr_reader :roster, :add_student, :grade,
+  
+  def initialize(roster)
+    @roster = {}
+  end
+  
+  def add_student(name, grade)
+    @add_student = add_student
+  end
+  
+  def grade(grade)
+    @grade = grade
+  end
+  
+  def sort
+    puts school.roster.sort
+  end
+end
+
