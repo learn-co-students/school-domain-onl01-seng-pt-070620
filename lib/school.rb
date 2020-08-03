94cd31f5ab9942bdce69b58187dd1edf0b6161d1
Fatou Diagne <f.m.diagne1@gmail.com>
2020-08-03 20:49:55 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,20 +1,31 @@
+require "pry"
+
 class School
-  attr_reader :roster, :add_student, :grade,
+  attr_reader :roster
   
-  def initialize(roster)
+  def initialize(name)
     @roster = {}
+    @name = name
   end
   
   def add_student(name, grade)
-    @add_student = add_student
+    @roster[grade] ||= [] # if what is on the left hand side of the or equals(||=) doen't exist, create it and set it equal to what is on the right side. If what is on the left hside does exist do nothing.
+    
+    @roster[grade] << name
   end
   
   def grade(grade)
-    @grade = grade
+    return @roster[grade]
   end
   
   def sort
-    puts school.roster.sort
+    collection = {}
+    @roster.each do |grade, students|
+     values = students.sort
+      collection[grade] = values
+    end
+    collection
   end
+  
 end
 
